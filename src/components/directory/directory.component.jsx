7fb67f9b2b31af03f14ed5457e60b90b3f981d62
huisheng <huisheng@ximnet.com.my>
2023-01-05 23:37:21 +0800

    wip

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -5,7 +5,7 @@ const Directory = ({categories} ) => {
  
 
   return (
-    <div className='categories-container'>
+    <div className='directory-container'>
 			{categories.map((category) => (
 				<CategoryItem key={category.id} category={category} />
         
