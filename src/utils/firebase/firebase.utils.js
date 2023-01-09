a8c1d8a07b2622a2a06885edb25757bb8e872e71
Hui Sheng <huisheng@ximnet.com.my>
2023-01-09 11:39:25 +0800

    added our sign in component as well as firebase utils and doc

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -34,4 +34,22 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
 export const db = getFirestore()
 export const createUserDocumentFromAuth = async (userAuth) => {
 	const userDocRef = doc(db, 'users', userAuth.uid)
+	//console.log(userDocRef)
+	const userSnapShot = await getDoc(userDocRef)
+	//console.log(userSnapShot)
+	//console.log(userSnapShot.exists())
+
+	//check if user exist
+	if (!userSnapShot.exists()) {
+		const { displayName, email } = userAuth
+		const createdAt = new Date()
+
+		try {
+			await setDoc(userDocRef, { displayName, email, createdAt })
+		} catch (error) {
+			console.log('Error creating user', error.message)
+		}
+	}
+
+	return userDocRef
 }
