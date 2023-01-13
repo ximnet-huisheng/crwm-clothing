f48e68ab05e83ad561865515da71d7f3098fcc5e
Hui Sheng <huisheng@ximnet.com.my>
2023-01-13 11:11:13 +0800

    Style signup form

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -4,6 +4,7 @@ import {
 	createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils'
 import FormInput from '../form-input/form-input.component'
+import './sign-up-form.styles.scss'
 const defaultFormFields = {
 	displayName: '',
 	email: '',
@@ -34,7 +35,8 @@ const SignUpForm = () => {
 
 	return (
 		<div className='sign-up-container'>
-			<h1>Sign up with email and password</h1>
+			<h2>Don't have an account?</h2>
+			<span>Sign up with email and password</span>
 			<form onSubmit={handleSubmit}>
 				<FormInput
 					label='Display Name'
