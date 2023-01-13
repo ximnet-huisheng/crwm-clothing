56594d9f6458fe2b8589e4a96f54d6be0b497b35
Hui Sheng <huisheng@ximnet.com.my>
2023-01-13 15:32:33 +0800

    Add user context

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import {
 	createUserDocumentFromAuth,
 	signInWithGooglePopup,
@@ -8,12 +8,16 @@ import FormInput from '../form-input/form-input.component'
 import Button from '../button/button.component'
 import './sign-in-form.styles.scss'
 
+import { UserContext } from '../../contexts/user.context'
+
 const defaultFormFields = {
 	email: '',
 	password: '',
 }
 
 const SignInForm = () => {
+	const { setCurrentUser } = useContext(UserContext)
+
 	const [formFields, setFormFields] = useState(defaultFormFields)
 	const { email, password } = formFields
 
@@ -36,8 +40,9 @@ const SignInForm = () => {
 		event.preventDefault()
 
 		try {
-			const response = await signInAuthUserWithEmailAndPassword(email, password)
-			console.log(response)
+			const { user } = await signInAuthUserWithEmailAndPassword(email, password)
+			setCurrentUser(user)
+
 			resetFormFields()
 		} catch (error) {
 			switch (error.code) {
---
+++ src/contexts/user.context.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState } from 'react'
+
+//as the actual value your want to access
+export const UserContext = createContext({
+	currentUser: null,
+	setCurrentUser: () => null,
+})
+
+//provider is the actual component
+export const UserProvider = ({ children }) => {
+	const [currentUser, setCurrentUser] = useState(null)
+	const value = { currentUser, setCurrentUser }
+	return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+}
