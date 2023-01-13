89973e568e3b7d7c8b2dd246127316f4bc86fefa
Hui Sheng <huisheng@ximnet.com.my>
2023-01-13 11:44:20 +0800

    Rename signin route to authentication

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -8,7 +8,7 @@ import {
 	createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils'
 
-import SignUpForm from './../../components/sign-up-form/sign-up-form.component'
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
 
 const SignIn = () => {
 	// useEffect(() => {
