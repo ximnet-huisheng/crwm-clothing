8894d38e2ee0e93077d5f77fc86b710ea0dcd6b6
Hui Sheng <huisheng@ximnet.com.my>
2023-01-13 11:42:00 +0800

    Add button component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -4,6 +4,7 @@ import {
 	createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils'
 import FormInput from '../form-input/form-input.component'
+import Button from '../button/button.component'
 import './sign-up-form.styles.scss'
 const defaultFormFields = {
 	displayName: '',
@@ -70,7 +71,7 @@ const SignUpForm = () => {
 					name='confirmPassword'
 					value={confirmPassword}
 				/>
-				<button type='submit'>Sign Up</button>
+				<Button type='submit'>Sign Up</Button>
 			</form>
 		</div>
 	)
