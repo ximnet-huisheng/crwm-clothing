ad7ad17c3651f3b93992df6e2e5c69c8af660b4f
huisheng <huisheng@ximnet.com.my>
2023-01-15 22:27:21 +0800

    wip

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,15 +1,16 @@
-import { ProviderId } from 'firebase/auth'
 import { createContext, useState } from 'react'
 import PRODUCTS from '../shop-data.json'
 
-export const ProductContext = createContext({
-	products: [],
+export const ProductsContext = createContext({
+	productsssss: [],
 })
 
 export const ProductsProvider = ({ children }) => {
 	const [products, setProducts] = useState(PRODUCTS)
-	const value = products
+	const value = { products }
 	return (
-		<ProductContext.Provider value={value}>{children}</ProductContext.Provider>
+		<ProductsContext.Provider value={value}>
+			{children}
+		</ProductsContext.Provider>
 	)
 }
