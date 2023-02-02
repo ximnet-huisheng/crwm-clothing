cc14faa06ebbe88292c43aa93cbf340d18b85d92
Hui Sheng <huisheng@ximnet.com.my>
2023-02-02 10:40:30 +0800

    Cart icon and dropdown

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,7 +6,7 @@ const CartIcon = () => {
 	return (
 		<div className='cart-icon-container'>
 			<ShoppingIcon className='shopping-icon' />
-			<span className='item-count'>0</span>
+			<span className='item-count'>10</span>
 		</div>
 	)
 }
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -5,6 +5,7 @@ import { UserContext } from '../../contexts/user.context'
 import './navigation.styles.scss'
 import { signOutUser } from '../../utils/firebase/firebase.utils'
 import CartIcon from './../../components/cart-icon/cart-icon.component'
+import CartDropDown from '../../components/cart-dropdown/cart-dropdown.component'
 
 const Navigation = () => {
 	const { currentUser } = useContext(UserContext)
@@ -12,7 +13,6 @@ const Navigation = () => {
 
 	return (
 		<Fragment>
-			<CartIcon />
 			<div className='navigation'>
 				<Link className='logo-container' to='/'>
 					<div>
@@ -33,7 +33,10 @@ const Navigation = () => {
 							SIGN IN
 						</Link>
 					)}
+
+					<CartIcon />
 				</div>
+				<CartDropDown></CartDropDown>
 			</div>
 			<Outlet />
 		</Fragment>
