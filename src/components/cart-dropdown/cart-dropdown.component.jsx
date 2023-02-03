2c85bb6191d736ed4e0b15a8f5503b34ea8c4906
Hui Sheng <huisheng@ximnet.com.my>
2023-02-03 15:41:27 +0800

    Add to cart part 1

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,9 +1,14 @@
 import './cart-dropdown.styles.scss'
 import Button from '../button/button.component'
+import CartItem from '../cart-item/cart-item.component'
 const CartDropDown = () => {
 	return (
 		<div className='cart-dropdown-container'>
-			<div className='cart-items' />
+			<div className='cart-items'>
+				{[].map(() => (
+					<CartItem></CartItem>
+				))}
+			</div>
 			<Button>GO TO CHECKOUT</Button>
 		</div>
 	)
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,12 +1,26 @@
 import { createContext, useState } from 'react'
 
+//helper function to check product
+const addCartItem = (cartItems, productToAdd) => {
+	//find if cartItem contain product
+	//if found, increment quantity
+	//return new array with modified cartItem / new cart item
+}
 export const CartContext = createContext({
 	isCartOpen: false,
 	setIsCartOpen: () => {},
+	cartItems: [],
+	addItemToCart: () => {},
 })
 
 export const CartProvider = ({ children }) => {
 	const [isCartOpen, setIsCartOpen] = useState(false)
+	const [cartItems, setCartItems] = useState([])
+
+	const addItemToCart = (productToAdd) => {
+		setCartItems(cartItems, productToAdd)
+	}
+
 	const value = { isCartOpen, setIsCartOpen }
 	return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
