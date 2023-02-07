460b403168869cf6d996fc722cef4e4183f64a45
Hui Sheng <huisheng@ximnet.com.my>
2023-02-07 17:26:54 +0800

    checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,16 @@
 import './cart-dropdown.styles.scss'
 import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { CartContext } from '../../contexts/cart.context'
 import Button from '../button/button.component'
 import CartItem from '../cart-item/cart-item.component'
 const CartDropDown = () => {
 	const { cartItems } = useContext(CartContext)
+
+	const navigate = useNavigate()
+	const goToCheckout = () => {
+		navigate('/checkout')
+	}
 	return (
 		<div className='cart-dropdown-container'>
 			<div className='cart-items'>
@@ -12,7 +18,7 @@ const CartDropDown = () => {
 					<CartItem key={item.id} cartItem={item}></CartItem>
 				))}
 			</div>
-			<Button>GO TO CHECKOUT</Button>
+			<Button onClick={goToCheckout}>GO TO CHECKOUT</Button>
 		</div>
 	)
 }
