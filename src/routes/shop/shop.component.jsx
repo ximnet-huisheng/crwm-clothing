5913b91978319d26bf84ac45e4b8abdfa0cab8ba
huisheng <huisheng@ximnet.com.my>
2023-02-13 00:02:12 +0800

    wip

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,11 +4,13 @@ import { Fragment, useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './shop.styles.scss'
 import CategoriesPreview from './../category-preview/category-preview.components'
+import Category from './../category/category.component'
 
 const Shop = () => {
 	return (
 		<Routes>
 			<Route index element={<CategoriesPreview></CategoriesPreview>}></Route>
+			<Route path=':category' element={<Category />}></Route>
 		</Routes>
 	)
 }
